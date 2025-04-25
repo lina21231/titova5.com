aadee3680317f8fc5ecd01dba2baf4cfaa437bc2
Alina <kn0ttt2006@gmail.com>
2025-04-25 10:32:00 +0300

    Реализована отправка контактной формы новая т к в старой забыла сохранить файлы

--- pages/contact.php
+++ pages/contact.php
@@ -1,11 +1,12 @@
 <?php include '../includes/header.php'; ?>
 <main>
     <h1>Контакты</h1>
-    <form method="post">
-        <input type="text" name="name" placeholder="Имя">
-        <input type="email" name="email" placeholder="Email">
-        <textarea name="message"></textarea>
+    <form id="contactForm" action="/equiv_nanka/includes/handle_contact.php" method="POST">
+        <input type="text" name="name" placeholder="Ваше имя" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <textarea name="message" placeholder="Сообщение"></textarea>
         <button type="submit">Отправить</button>
     </form>
+    <div id="formResponse"></div>
 </main>
 <?php include '../includes/footer.php'; ?>
