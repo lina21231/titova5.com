c8ea1c2338e822f537e6b0d97d4e47b7d638752b
Alina <kn0ttt2006@gmail.com>
2025-04-25 10:14:57 +0300

    Добавлен favicon для сайта

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой Сайт</title>
+    <link rel="icon" href="/equiv_nanka/assets/images/favicon.ico" type="image/x-icon">
+</head>
 <?php
 $menuItems = [
     'Главная' => '/index.php',
